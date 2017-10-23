63e24a33a193053fe5f8904221b0079ece7d6905
monsterchild <jeremydejiacomo@gmail.com>
2017-10-23 18:55:56 -0400

    in progress

--- 09/thermostat/js/index.js
+++ 09/thermostat/js/index.js
@@ -1,2 +1,34 @@
+/* variables */
+var currentTemp = 71;
+var currentTempElement = document.getElementById('currentTemp'); //Object
+var inputField = document.getElementById('newTemp'); //Object
+var button = document.getElementById('setTemperature'); //Object
+var body = document.getElementsByTagName('body');
 
-  
+/* logs */
+console.dir('currentTempElement');
+console.log('button');
+
+/* logic statements */
+currentTempElement.innerHTML = currentTemp;
+changeBackground(currentTemp);
+
+button.onclick = function () {
+	currentTemp = inputField.value;
+	if (currentTemp !== '') {
+		currentTempElement.innerHTML = currentTemp; 
+		changeBackground(currentTemp);
+	}
+};
+
+function changeBackground(temp) {
+	if (temp <= 65) {
+		console.log('blue');
+	} else if (temp > 65 && temp < 85) {
+		body[0].style.backgroundcolor = "yellow";
+	} else if (temp >= 85 && temp < 95) {
+		console.log('orange');
+	} else {
+		console.log('red');
+	}
+};
